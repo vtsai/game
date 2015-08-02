15095c7d722d81970c336828891d7e4a0705a7b0
Victor <vytsai@gmail.com>
2015-08-01 22:56:42 -0700

    server

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ var express = require('express');
  
 var server = express();
 var oneDay = 86400000;
-process.env.PWD = process.cwd()
-
-//server.use(express.static(__dirname + '../dist', { maxAge: oneDay }));
-server.use(express.static(process.env.PWD + '/dist', { maxAge: oneDay }));
-
-server.get('/', function (req, res) {
-//  res.sendFile(__dirname+'../dist/index.html');
-  res.sendFile(process.env.PWD+'/dist/index.html');
-});
- 
 var port = process.env.PORT || 8080;
-server.listen(port, function() {
-    console.log('server listening on port ' + port);
-});
+
+server.get('/', function(request, response) {
+    response.sendfile(__dirname + '/dist/index.html');
+}).configure(function() {
+    server.use('/', express.static(__dirname + '/dist/'));
+}).listen(port);
