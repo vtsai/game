2be7345d5b3d8d33992614d7b8cc9c3264a9dc6f
Victor <vytsai@gmail.com>
2015-08-01 22:37:35 -0700

    specify node and nmp versions

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var server = express();
 var oneDay = 86400000;
 
-server.use(express.static(__dirname + '/dist', { maxAge: oneDay }));
+server.use(express.static(__dirname + '../dist', { maxAge: oneDay }));
 //server.use(express.static(__dirname + '/mewzy'));
 
-//server.get('/', function (req, res) {
+server.get('/', function (req, res) {
 //  res.send(__dirname+'/dist');
-//  res.sendFile(__dirname+'/dist/index.html');
-//});
+  res.sendFile(__dirname+'../dist/index.html');
+});
  
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
