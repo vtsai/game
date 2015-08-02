7f94dc6f32cfc3c377bb5e0638b000586b08e186
Victor <vytsai@gmail.com>
2015-08-01 23:25:30 -0700

    dist to app

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
 
-server.use(express.static(__dirname + '/'));
+server.use(express.static(__dirname + '/app'));
 
 server.get('/', function (req, res) {
-  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/app/index.html');
 });
  
 server.listen(port, function() {
