2c84d21e2ebfc918b350691ada7ac8a4662a932b
Victor <vytsai@gmail.com>
2015-08-01 21:49:49 -0700

    dist index

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ server.use(express.static(__dirname + '/dist', { maxAge: oneDay }));
 //server.use(express.static(__dirname + '/mewzy'));
 
 server.get('/', function (req, res) {
-  res.send(__dirname+'/dist');
-//  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/dist/index.html');
 });
  
 var port = process.env.PORT || 8080;
