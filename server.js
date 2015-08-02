3c323c549ca2276eace38b001951f696290a7241
Victor <vytsai@gmail.com>
2015-08-01 23:39:43 -0700

    procfile

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
 
-server.use(express.static(__dirname + '/app'));
+server.use(express.static(__dirname + '/'));
 
 server.get('/', function (req, res) {
-  res.sendFile(__dirname+'/app/index.html');
+  res.sendFile(__dirname+'/index.html');
 });
  
 server.listen(port, function() {
