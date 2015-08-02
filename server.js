e2d857e946a5c8a18ba133da336e366f89f2a3d4
Victor <vytsai@gmail.com>
2015-08-01 17:50:43 -0700

    server get

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var oneDay = 86400000;
 
 server.use(express.static(__dirname + '/dist', { maxAge: oneDay }));
 //server.use(express.static(__dirname + '/mewzy'));
+
+server.get('/', function (req, res) {
+//  res.send(__dirname+'/mewzy');
+  res.sendFile(__dirname+'/index.html');
+});
  
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
