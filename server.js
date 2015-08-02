2134f8bf7164da7bcf70e6902f87e3fee0dfc814
Victor <vytsai@gmail.com>
2015-08-01 22:32:38 -0700

    specify node and nmp versions

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var oneDay = 86400000;
 server.use(express.static(__dirname + '/dist', { maxAge: oneDay }));
 //server.use(express.static(__dirname + '/mewzy'));
 
-server.get('/', function (req, res) {
-  res.sendFile(__dirname+'/dist/index.html');
-});
+//server.get('/', function (req, res) {
+//  res.send(__dirname+'/dist');
+//  res.sendFile(__dirname+'/dist/index.html');
+//});
  
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
