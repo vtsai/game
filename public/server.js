fb8479d836ab9697e3e37d2e026de55a9f084513
Victor <vytsai@gmail.com>
2015-08-02 14:52:37 -0700

    public

---
+++ public/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+ 
+var server = express();
+var port = process.env.PORT || 8080;
+
+//server.use(express.static(__dirname + '/'));
+
+server.use(express.static(__dirname + '/public'));
+
+//server.get('/', function (req, res) {
+//  res.sendFile(__dirname+'/index.html');
+//});
+ 
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
