dd142122b002c7783079abaa0a7539ee5b497bfc
Victor <vytsai@gmail.com>
2015-08-02 14:22:08 -0700

    node

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.use(express.static(__dirname + '/app'));
 
 server.get('/', function (req, res) {
 //  res.sendFile(__dirname+'/index.html');
-  res.redirect('/index.html');
+  res.redirect('/');
 });
  
 server.listen(port, function() {
