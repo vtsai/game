30c832e6755f1a12eace9ade4e4efb6568723898
Victor <vytsai@gmail.com>
2015-08-01 17:46:49 -0700

    Node server fix

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 var express = require('express');
-var app = express();
+ 
+var server = express();
+var oneDay = 86400000;
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
+server.use(express.static(__dirname + '/dist', { maxAge: oneDay }));
+//server.use(express.static(__dirname + '/mewzy'));
+ 
 var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
 });
