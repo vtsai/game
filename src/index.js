fb01c21595e3617292bcb03fd762deca3f20b182
McRockJr <legacybuilders.develop@hotmail.com>
2015-08-09 15:40:20 -0700

    orange box

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ window.onload = function() {
     Crafty.init();
     // A blue block, controlled by arrow keys
     var player = Crafty.e('2D, Canvas, Color, Fourway')
-        .attr({x:100, y:100, w:50, h:50})
-        .color('blue')
+        .attr({x:100, y:100, w:58, h:58})
+        .color('orange')
         .fourway(3);
 };
