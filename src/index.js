82e7a58cba3818842a31b76afd0f61cde428005d
Victor <vytsai@gmail.com>
2015-08-09 14:58:32 -0700

    eslint

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import 'craftyjs';
 window.onload = function() {
     Crafty.init();
     // A blue block, controlled by arrow keys
-    var player = Crafty.e("2D, Canvas, Color, Fourway")
+    var player = Crafty.e('2D, Canvas, Color, Fourway')
         .attr({x:100, y:100, w:50, h:50})
-        .color("blue")
+        .color('blue')
         .fourway(3);
 };
